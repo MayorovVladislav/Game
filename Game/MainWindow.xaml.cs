69962f287aa3182968065d3e094dee156f282fba
MayorovVladislav <Mayorovvladislav@outlook.com>
2016-05-05 18:02:31 +0300

    Игра

---
+++ Game/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Game
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        WhatNumberLibrary.WhatNumber gamenumber;
+        public MainWindow()
+        {
+            InitializeComponent();
+            MenuItem_Click(null, null);
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox.Text != "")
+                gamenumber.Check(int.Parse(textBox.Text));
+            else
+                MessageBox.Show("Введите число!", "Ошибка.", MessageBoxButton.OK);
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            gamenumber = new WhatNumberLibrary.WhatNumber();
+            gamenumber.message += Gamenumber_message;
+            gamenumber.gameover += Gamenumber_gameover;
+        }
+
+        private void Gamenumber_gameover(object sender, WhatNumberLibrary.WhatNumber.EventMessage e)
+        {
+            MessageBoxResult d = MessageBox.Show(e.Message, "Игра окончена", MessageBoxButton.YesNo);
+            if (d == MessageBoxResult.Yes)
+            {
+                MenuItem_Click(null, null);
+                textBox.Text = "";
+            }
+            else Close();
+        }
+
+        private void Gamenumber_message(object sender, WhatNumberLibrary.WhatNumber.EventMessage e)
+        {
+            MessageBox.Show(e.Message);
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void textBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!char.IsDigit(e.Text, 0)) { e.Handled = true; return; }
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox tex = (TextBox)sender;
+            if (tex.Text.Contains(" "))
+                tex.Text = tex.Text.Trim();
+            if (tex.Text != string.Empty)
+                if (int.Parse(tex.Text) > 100)
+                    tex.Text = string.Empty;
+        }
+    }
+}
---
+++ WhatNumberLibrary/WhatNumber.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatNumberLibrary
+{
+    /// <summary>
+    /// Логика игры "Угадай число".
+    /// </summary>
+    public class WhatNumber
+    {
+        /// <summary>
+        /// Класс для вывода сообщений.
+        /// </summary>
+        public class EventMessage : EventArgs
+        {
+            /// <summary>
+            /// Сообщение для вывода.
+            /// </summary>
+            public string Message { get; private set; }
+
+            /// <summary>
+            /// Конструктор для сохранения сообщения.
+            /// </summary>
+            /// <param name="message"></param>
+            public EventMessage(string message)
+            {
+                Message = message;
+            }
+        }
+
+        int counter = 3;
+        int random;
+        /// <summary>
+        /// Создание новой игры.
+        /// </summary>
+        public WhatNumber()
+        {
+            random = new Random().Next(0, 101);
+        }
+        /// <summary>
+        /// Проверка введенного значения.
+        /// </summary>
+        /// <param name="number">Пользовательское число.</param>
+        public void Check(int number)
+        {
+            EventMessage eventmessage = new EventMessage("");
+
+            if (number <= 100 || number >= 0)
+            {
+                counter--;
+                if (counter != 0)
+                {
+                    if (number > random)
+                        eventmessage = new EventMessage($"Ваше число больше загаданного!\nОсталось попыток: {counter }");
+                    else if (number < random)
+                        eventmessage = new EventMessage($"Ваше число меньше загаданного!\nОсталось попыток: {counter }");
+                    else
+                    {
+                        eventmessage = new EventMessage("Вы угадали число!\nХотите начать новую игру?");
+                        GemeOverOutPut(eventmessage);
+                        return;
+                    }
+                }
+                else
+                {
+                    if (number == random)
+                        eventmessage = new EventMessage("Вы угадали число!\nХотите начать новую игру?");
+                    else
+                        eventmessage = new EventMessage("Вы не угадали число!\nХотите начать новую игру?");
+                    GemeOverOutPut(eventmessage);
+                    return;
+                }
+            }
+            else
+                eventmessage = new EventMessage("Пожалуйства введите число в пределах 0 - 100.");
+            OutPut(eventmessage);
+        }
+
+        /// <summary>
+        /// Происходит при поиске случайного числа.
+        /// </summary>
+        public event EventHandler<EventMessage> message;
+        /// <summary>
+        /// Происходит при последней попытке и выводит сообщение об окончании игры.
+        /// </summary>
+        public event EventHandler<EventMessage> gameover;
+
+        /// <summary>
+        /// Вызов события.
+        /// </summary>
+        /// <param name="eventmessage"></param>
+        private void OutPut(EventMessage eventmessage)
+        {
+            if (message != null)
+                message(this, eventmessage);
+        }
+        /// <summary>
+        /// Вызов события.
+        /// </summary>
+        /// <param name="eventmessage"></param>
+        private void GemeOverOutPut(EventMessage eventmessage)
+        {
+            if (gameover != null)
+                gameover(this, eventmessage);
+        }
+    }
+}
+
